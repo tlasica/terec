7195ced0cd87f168159a85a70753ff20a39b77ea
Tomek Lasica <tlasica@life.pl>
2023-10-17 19:06:39 +0200

    Try pytest-docker-fixtures
    This basically does not work without tweaks.
    The problem is that cassandra is not a support image
    and we need to add some implementation.
    
    I tries to start cassandra but does not really work:
    cassandra is not starting.
    
    So instead we should try some docker compose approach.

--- test/components/terec/database/test_core.py
+++ test/components/terec/database/test_core.py
@@ -1,7 +1,7 @@
 from terec.database import cassandra_session
 
 
-def test_cassandra_connection_with_default_settings():
+def test_cassandra_connection_with_default_settings(cassandra):
     conn = cassandra_session()
     assert not conn.is_shutdown, "Connection is in shutdown state."
 
---
+++ test/conftest.py
@@ -0,0 +1,51 @@
+import time
+
+import pytest
+from pytest_docker_fixtures import images
+from pytest_docker_fixtures.containers._base import BaseImage
+from tenacity import retry, wait_fixed, stop_after_attempt
+
+#
+# Configure cassandra image based on
+# https://github.com/guillotinaweb/pytest-docker-fixtures
+#
+images.settings['cassandra'] = {}
+images.configure(
+    'cassandra',
+    'cassandra', 'latest',
+    env={},
+    options={
+        'ports': {
+            '9042': '9042'
+        }
+    }
+)
+
+
+class CassandraImage(BaseImage):
+    name = 'cassandra'
+
+    def check(self):
+        from cassandra.cluster import ConnectionException, NoHostAvailable
+        try:
+            self._check_connection()
+            return True
+        except (ConnectionException, NoHostAvailable):
+            return False
+
+    @retry(wait=wait_fixed(5), stop=stop_after_attempt(10))
+    def _check_connection(self):
+        print("trying to connect to cassandra")
+        from cassandra.cluster import Cluster
+        cluster = Cluster()
+        cluster.connect()
+
+
+
+cassandra_image = CassandraImage()
+
+
+@pytest.fixture(scope='session')
+def cassandra():
+        yield cassandra_image.run()
+        cassandra_image.stop()
