9ad0cf0f2e3d706bf56bbc6538ea0102a41997f5
Tomek Lasica <tlasica@life.pl>
2023-10-24 09:18:06 +0200

    Add --keepalive pytest flag to not start docker compose again.
    The approach is from this comment:
    https://github.com/avast/pytest-docker/issues/46#issuecomment-887408396
    
    poetry run pytest --keepalive should do the trick.

--- test/conftest.py
+++ test/conftest.py
@@ -8,6 +8,49 @@ from terec.database import cassandra_session
 from terec.model.projects import Org, Project
 from terec.model.util import cqlengine_init
 
+# This is to add --keepalive flag when running pytests so that we do not start docker again
+# from https://github.com/avast/pytest-docker/issues/46#issuecomment-887408396
+
+
+def pytest_addoption(parser):
+    """Add custom options to pytest.
+    Add the --keepalive option for pytest.
+    """
+
+    parser.addoption("--keepalive", "-K", action="store_true", default=False, help="Keep Docker containers alive")
+
+
+@pytest.fixture(scope="session")
+def keepalive(request):
+    """Check if user asked to keep Docker running after the test."""
+
+    return request.config.option.keepalive
+
+
+@pytest.fixture(scope="session")
+def docker_compose_project_name(keepalive, docker_compose_project_name):
+    """
+    Override `docker_compose_project_name` to make sure that we have a unique project name
+    if user asked to keep containers alive. This way we won’t create Docker container every time we will start pytest.
+    """
+
+    if keepalive:
+        return "pytest-terec"
+
+    return docker_compose_project_name
+
+
+@pytest.fixture(scope="session")
+def docker_cleanup(keepalive, docker_cleanup):
+    """
+    If user asked to keep Docker alive, make `pytest-docker` execute the `docker-compose version` command.
+    This way, Docker container won’t be shut down."""
+
+    if keepalive:
+        return "version"
+
+    return docker_cleanup
+
 
 @pytest.fixture(scope="session")
 def docker_compose_file(pytestconfig):
