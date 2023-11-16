f16123e9f03b682595ec45ffe0220498128e1e18
Tomek Lasica <tlasica@life.pl>
2023-11-16 14:52:54 +0100

    Skip tests if precondition is not met

--- test/bases/terec/jenkins_cli/test_pipeline_commands.py
+++ test/bases/terec/jenkins_cli/test_pipeline_commands.py
@@ -1,3 +1,7 @@
+import os
+
+import pytest
+
 from terec.jenkins_cli import main
 from typer.testing import CliRunner
 
@@ -5,12 +9,14 @@ from typer.testing import CliRunner
 runner = CliRunner()
 
 
+@pytest.mark.skipif("JENKINS_URL" in os.environ, reason="JENKINS_URL is set")
 def test_build_export_fails_without_jenkins_url():
     result = runner.invoke(main.app, ["pipeline", "export-build", "job-name", "3"])
     assert result.exit_code != 0
     assert "JENKINS_URL is not set" in str(result.exception)
 
 
+@pytest.mark.skipif("JENKINS_URL" in os.environ, reason="JENKINS_URL is set")
 def test_tests_export_fails_without_jenkins_url():
     result = runner.invoke(main.app, ["pipeline", "export-tests", "job-name", "3"])
     assert result.exit_code != 0
