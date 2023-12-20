ecdb219533dbe02ae1d06afdd9502e9e8c81cb87
Tomek Lasica <tlasica@life.pl>
2023-12-20 17:31:25 +0100

    Use TEREC_URL in import scripts

--- development/import-cassandra-3.11.sh
+++ development/import-cassandra-3.11.sh
@@ -9,9 +9,9 @@ BUILD_NUM=$1
 
 # collect and import suite run
 python bases/terec/jenkins_cli/main.py pipeline export-build Cassandra-3.11 ${BUILD_NUM} --suite ${SUITE} | tee suite.json
-cat suite.json | http localhost:8000/tests/orgs/${TEREC_ORG}/runs
+cat suite.json | http ${TEREC_URL}/tests/orgs/${TEREC_ORG}/runs
 
 # collect and import tests
 python bases/terec/jenkins_cli/main.py pipeline export-tests Cassandra-3.11 ${BUILD_NUM} > tests.json
-cat tests.json | http localhost:8000/tests/orgs/${TEREC_ORG}/projects/${TEREC_PRJ}/suites/${SUITE}/runs/${BUILD_NUM}/tests
+cat tests.json | http ${TEREC_URL}/tests/orgs/${TEREC_ORG}/projects/${TEREC_PRJ}/suites/${SUITE}/runs/${BUILD_NUM}/tests
 
