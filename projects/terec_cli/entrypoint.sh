91229ddccf0f30528d21a48f233a5721118b3f2e
Tomasz Lasica <tlasica@life.pl>
2025-05-12 21:00:13 +0200

    Create terec/cli docker image with terec cli

--- projects/terec_cli/entrypoint.sh
+++ projects/terec_cli/entrypoint.sh
@@ -1,21 +1,3 @@
 #!/bin/bash
 
-# Direct command based on alias logic
-case "$1" in
-  import)
-    shift
-    exec poetry run python bases/terec/import_cli/main.py "$@"
-    ;;
-  status)
-    shift
-    exec poetry run python bases/terec/status_cli/main.py "$@"
-    ;;
-  jenkins)
-    shift
-    exec poetry run python bases/terec/jenkins_cli/main.py "$@"
-    ;;
-  *)
-    echo "Unknown command: $1"
-    exit 1
-    ;;
-esac
+exec python -m terec.cli.main "$@"
